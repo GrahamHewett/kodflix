d89f87bf26463936f1205940b352d260097f41da
Graham Hewett <hewettgraham@gmail.com>
2018-11-08 10:15:17 +0000

    add synopsis and image divs to details page

--- src/details/Details.js
+++ src/details/Details.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import movieData from '../movie-data'
 import { Redirect } from "react-router-dom";
+import './details.css'
 
 export default class StatefulDetails extends Component {
   constructor(props) {
     super(props);
     //Must initialise state first
     this.state = {
-      title: ''
+      title: '',
+      synopsis: 'Default Synopsis',
+      image: 'Default Image',
     }
   }
   //Method to alter the state
-
   componentDidMount() {
-      let found = movieData.find((movie) => movie.id === this.props.match.params.id)
-      let name = found ? found.title : <Redirect to='/not-found' />
-      this.setState({ title: name });
-    };
+    let found = movieData.find((movie) => movie.id === this.props.match.params.id)
+    let name = found ? found.title : <Redirect to='/not-found' />
+    let description = found ? found.synopsis : "No Synopsis Given"
+    let picture = found ? <img src={found.imgSrc} alt="Movie Cover"></img> : "No Image Available"
 
-render() {
-  return (
-    <div>
-      <h1>{this.state.title}</h1>
-    </div>
-  );
-}
+      this.setState({ 
+        title: name,
+        synopsis: description,
+        image: picture 
+      });
+
+  };
+
+  render() {
+    return (
+      <div id="container">
+        <div className="title"><h1>{this.state.title}</h1></div>
+        <div className="synopsis"><h1>{this.state.synopsis}</h1></div>
+        <div className="image"><h1>{this.state.image}</h1></div>
+      </div>
+    );
+  }
 };
--- src/movie-data.js
+++ src/movie-data.js
@@ -5,10 +5,12 @@ import film4 from './gameOfThrones.jpg';
 import film5 from './theWire.jpg';
 import film6 from './theWalkingDead.jpg';
 
+const breakingBadSynopsis = "Write a lovely breaking bad synopsis here"
+
 let movieData =
     [
-        { title: "Black Mirror", imgSrc: film1, id: "black-mirror", },
-        { title: "Breaking Bad", imgSrc: film2, id: "breaking-bad", },
+        { title: "Black Mirror", imgSrc: film1, id: "black-mirror", synopsis: 'black mirror synopsis' },
+        { title: "Breaking Bad", imgSrc: film2, id: "breaking-bad", synopsis: breakingBadSynopsis},
         { title: "Death Note", imgSrc: film3, id: "death-note", },
         { title: "Game of Thrones", imgSrc: film4, id: "game-of-thrones", },
         { title: "The Wire", imgSrc: film5, id: "the-wire", },
@@ -16,3 +18,4 @@ let movieData =
     ]
 
 export default movieData;
+
