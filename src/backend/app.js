5b8e2a1251cc98f903cd0cf32624ad95a0114cc2
Graham Hewett <hewettgraham@gmail.com>
2018-11-13 14:09:56 +0000

    remember to require path

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const shows = require('./shows');
 const app = express()
+const path = require('path');
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.send('Hello Graham!'))
 app.get('/rest/shows', (req, res) => res.send(shows.data))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.get('*', function (req, res) {
+res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 //Serve any static files from the build folder
 app.use(express.static(path.join(__dirname, '../../build')));
 // Handle React routing, return all requests to React app
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
