f56d2382769c7c66639c48914dfdfd519d5180b2
Graham Hewett <hewettgraham@gmail.com>
2018-11-20 16:25:31 +0000

    add google analytics

--- src/backend/db.js
+++ src/backend/db.js
@@ -4,7 +4,7 @@ const url = 'mongodb://localhost:27017'
 module.exports = { connect };
 
 function connect() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         MongoClient.connect(url, function(err, mongoDB) {
             if (err) throw err;
             const dbo = mongoDB.db("kodflix");
--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import Routes from './navigation/Routes';
 import './App.css';
+import ReactGA from 'react-ga';
+
 
 class App extends Component {
+
+  constructor({ history, location }) {
+    super();
+    ReactGA.initialize('UA-129505001-1');
+    this.trackPageView(location);
+    history.listen((location) => this.trackPageView(location));
+  }
+   trackPageView(location) {
+    ReactGA.pageview(location.pathname + location.search + location.hash);
+  }
 
   render() {
     return (
