a099d86d2eb44ff25d574bfc24734f4e4b2da833
Graham Hewett <hewettgraham@gmail.com>
2018-11-26 13:36:48 +0000

    connect google analytics

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import Routes from './navigation/Routes';
 import './App.css';
 import ReactGA from 'react-ga';
-import Menu from './navigation/Menu' 
-
+import Menu from './navigation/Menu'
+import { createBrowserHistory } from "history";
 
 class App extends Component {
 
   constructor() {
     super();
     ReactGA.initialize('UA-129505001-1');
+    const history = createBrowserHistory();
+    const location = history.location;
+    this.trackPageView(location);
+    history.listen((location) => this.trackPageView(location))
+  }
+
+  trackPageView(location) {
+    ReactGA.pageview(location.pathname + location.search + location.hash)
   }
 
   render() {
