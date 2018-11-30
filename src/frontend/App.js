de9ce4dcb0a8d9d8452c848ce1f3285478413d17
Graham Hewett <hewettgraham@gmail.com>
2018-11-30 13:06:26 +0000

    GA WORKS !!!!!!

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -3,29 +3,14 @@ import Routes from './navigation/Routes';
 import './App.css';
 import ReactGA from 'react-ga';
 import Menu from './navigation/Menu'
-import { createBrowserHistory } from "history";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-
-    ReactGA.initialize('UA-129505001-1', {
-    gaOptions: {
-      siteSpeedSampleRate: 100
-    }
-    });
-
-    const history = createBrowserHistory();
-    const location = history.location;
-    this.trackPageView(location);
-    history.listen((location) => this.trackPageView(location))
-  }
-
-  trackPageView(location) {
-    // ReactGA.pageview(location.pathname + location.search + location.hash)
-    ReactGA.pageview(window.location.pathname + window.location.search + window.location.hash);
-
+    ReactGA.initialize('UA-129505001-1')
+    const location = window.location;
+    ReactGA.pageview(location.pathname + location.search + location.hash);
   }
 
   render() {
