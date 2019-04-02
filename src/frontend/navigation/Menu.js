a7334f0c6509dffa44ce145fcda6d91e5838a55d
Graham Hewett <hewettgraham@gmail.com>
2019-04-02 18:52:00 +0100

    heroku test

--- src/frontend/navigation/Menu.js
+++ src/frontend/navigation/Menu.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import './menu.css';
 import { Link } from "react-router-dom";
-import homeIcon from '../../common/images/home2.svg'
+import homeIcon from '../../common/images/home.svg'
 import tvIcon from '../../common/images/tvIcon.svg'
-
 
 export default class Menu extends React.Component {
   constructor() {
