6ee660f85a94d02c3d9893e3df6d0b83140fea99
Graham Hewett <hewettgraham@gmail.com>
2019-12-13 12:56:07 +0000

    functional route component

--- src/frontend/navigation/Routes.js
+++ src/frontend/navigation/Routes.js
@@ -5,17 +5,13 @@ import Details from '../details/Details';
 import Play from '../details/Play';
 import NotFound from '../navigation/NotFound';
 
-export default class Routes extends React.Component {
-  render() {
-    return (
-      <div id='views'>
-        <Switch>
+export default function Routes() {
+    return <div id='views'>
+      <Switch>
         <Route path="/" component={Gallery} exact />
         <Route path="/not-found"component={NotFound} exact />
         <Route path="/:id/play" component={Play} exact />
         <Route path="/:id" component={Details} exact />
-        </Switch>
-      </div>
-    )
-  }
+      </Switch>
+    </div>
 };
